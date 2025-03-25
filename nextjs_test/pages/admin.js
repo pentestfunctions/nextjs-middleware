8cdc017ccc9092e8a3cb03a43bbaf3d884888b58
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2025-03-25 19:30:48 +1300

    Create admin.js

---
+++ nextjs_test/pages/admin.js
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <p>This is a protected page.</p>
+      <p>Your flag is TEST_FLAG_HERE</p>
+    </div>
+  );
+}
